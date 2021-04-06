049159547c2bd9bc54185558cf6c917f46722e55
juplmn777 <julien.plumyoen@gmail.com>
2021-04-06 10:05:00 +0200

    responsive canvas

---
+++ app.js
@@ -0,0 +1,16 @@
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener('resize', () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  draw();
+});
+
+const draw = () => {};
+
+draw();
