0c65e1b05e1b7ece633a841f711d00323b5231b3
juplmn777 <julien.plumyoen@gmail.com>
2021-04-07 07:54:08 +0200

    sun, moon & earth variables created with src

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ window.addEventListener('resize', () => {
   draw();
 });
 
+const sun = new Image();
+const moon = new Image();
+const earth = new Image();
+
+sun.src = 'https://mdn.mozillademos.org/files/1456/Canvas_sun.png';
+moon.src = 'https://mdn.mozillademos.org/files/1443/Canvas_moon.png';
+earth.src = 'https://mdn.mozillademos.org/files/1429/Canvas_earth.png';
+
 const draw = () => {};
 
 draw();
