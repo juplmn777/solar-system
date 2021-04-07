7b06834968b4fff46b3fe9c541c352063077160f
juplmn777 <julien.plumyoen@gmail.com>
2021-04-07 09:07:12 +0200

    milliSeconds added

--- app.js
+++ app.js
@@ -35,14 +35,20 @@ const draw = () => {
   //earth & earth-rotating on orbit
   const time = new Date();
   //console.log(time);
-  context.rotate(((2 * Math.PI) / 60) * time.getSeconds());
+  context.rotate(
+    ((2 * Math.PI) / 60) * time.getSeconds() +
+      ((2 * Math.PI) / 60000) * time.getMilliseconds()
+  );
 
   //earth on orbit
   context.translate(105, 0);
   context.drawImage(earth, -12, -12); //-12: earth center on orbit
 
   //moon
-  context.rotate(((2 * Math.PI) / 6) * time.getSeconds());
+  context.rotate(
+    ((2 * Math.PI) / 6) * time.getSeconds() +
+      ((2 * Math.PI) / 6000) * time.getMilliseconds()
+  );
   context.translate(0, 28);
 
   context.drawImage(moon, -3.5, -3.5);
@@ -51,7 +57,7 @@ const draw = () => {
 
   //sun
   context.drawImage(sun, 0, 0);
-  requestAnimationFrame(draw);
+  window.requestAnimationFrame(draw);
 };
 
 draw();
