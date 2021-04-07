3d65cfc9397161c3dc03c656212a939f5e9717ee
juplmn777 <julien.plumyoen@gmail.com>
2021-04-07 08:48:15 +0200

    earth orbit drawn

--- app.js
+++ app.js
@@ -21,6 +21,16 @@ earth.src = 'https://mdn.mozillademos.org/files/1429/Canvas_earth.png';
 
 const draw = () => {
   context.globalCompositeOperation = 'destination-over'; //Les nouvelles formes sont dessinées derrière le contenu existant.
+  context.clearRect(0, 0, 300, 300);
+
+  //earth orbit
+  context.beginPath();
+  context.strokeStyle = 'crimson';
+
+  context.arc(150, 150, 105, 0, Math.PI * 2);
+  context.stroke();
+
+  context.save();
 };
 
 draw();
